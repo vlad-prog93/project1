976bb4944c951f123e83dca50a5703b34eb6262e
vlad-prog93 <vladka_@bk.ru>
2021-01-28 09:48:13 +0300

    Обработка словарей и списков

--- Project3.py
+++ Project3.py
@@ -32,12 +32,14 @@ for i in range(count_users):
  except TypeError:
   print(f'Значение ключа name для пользователя {i+1}: {name_user}','\n')
 
+# среднее значение возраста пользователей
+years = (user1["age"] + user2["age"] + user3["age"] + user4["age"])/2
+print(f'Среднее значение возраста: {years} ')
+
 # выбор пользователя
 try:
  count_number = int(input('Введите порядковый номер пользователя (от 1 до 4): '))
  print('')
- years = (user1["age"] + user2["age"] + user3["age"] + user4["age"])/2
- print(f'Среднее значение возраста: {years} ')
  choose_user = user_list[count_number - 1]
  print(f'''
 
